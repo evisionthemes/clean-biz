1de8321e992ba1c18e783b2518d1d48e98bb8a9d
pradip <cloudpradip@gmail.com>
2017-06-18 12:59:51 +0545

    changes

--- front-page.php
+++ front-page.php
@@ -20,25 +20,28 @@ else{
 	<?php  
 	$clean_biz_selected_page = absint($clean_biz_customizer_all_values['clean-biz-enable-selected-page']);
 	if ($clean_biz_selected_page == 1) { ?>
-			<div id="primary" class="content-area col-sm-8">
-			    <main id="main" class="site-main" role="main">
+		    <div class="container">
+				<div id="primary" class="content-area col-sm-8">
+				    <main id="main" class="site-main" role="main">
 
-			        <?php
-			        while ( have_posts() ) : the_post();
+				        <?php
+				        while ( have_posts() ) : the_post();
 
-			            get_template_part( 'template-parts/content', 'page' );
+				            get_template_part( 'template-parts/content', 'page' );
 
-			            // If comments are open or we have at least one comment, load up the comment template.
-			            if ( comments_open() || get_comments_number() ) :
-			                comments_template();
-			            endif;
+				            // If comments are open or we have at least one comment, load up the comment template.
+				            if ( comments_open() || get_comments_number() ) :
+				                comments_template();
+				            endif;
 
-			        endwhile; // End of the loop.
-			        ?>
+				        endwhile; // End of the loop.
+				        ?>
 
-			    </main><!-- #main -->
-			</div><!-- #primary -->
-		<?php get_sidebar(); ?>
+				    </main><!-- #main -->
+				</div><!-- #primary -->
+				<?php get_sidebar(); ?>
+				</div>
+		
 
 	<?php
 	}
